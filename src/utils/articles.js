a1a3d67937ac3e5e8f7704a27a9f6b458b83cafb
Priscilla-D <durand.priscilla@outlook.fr>
2023-08-22 23:13:31 +0200

    export articles infos

---
+++ src/utils/articles.js
@@ -0,0 +1,42 @@
+export const ARTICLES_INFOS = [
+  {
+    title: "Tibet Adventure",
+    src: "./images/tour-1.jpeg",
+    date: "august 26th, 2020",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "china",
+    duration: "6 days",
+    price: "from $2100",
+  },
+  {
+    title: "best of java",
+    src: "./images/tour-2.jpeg",
+    date: "october 1th, 2020",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "indonesia",
+    duration: "11 days",
+    price: "from $1400",
+  },
+  {
+    title: "explore hong kong",
+    src: "./images/tour-3.jpeg",
+    date: "september 15th, 2020",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "hong kong",
+    duration: "8 days",
+    price: "from $5000",
+  },
+  {
+    title: "kenya highlights",
+    src: "./images/tour-4.jpeg",
+    date: "december 5th, 2019",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "kenya",
+    duration: "20 days",
+    price: "from $3300",
+  },
+];
