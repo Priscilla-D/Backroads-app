a611e84e431c29d76aa430b9a6da84a7c484c34e
Priscilla-D <durand.priscilla@outlook.fr>
2023-08-22 23:12:57 +0200

    factorize articles

--- src/Tours.js
+++ src/Tours.js
@@ -1,3 +1,5 @@
+import { ARTICLES_INFOS } from "./utils/articles";
+
 function Tours() {
   return (
     <section className="section" id="tours">
@@ -8,107 +10,32 @@ function Tours() {
       </div>
 
       <div className="section-center featured-center">
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">august 26th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Tibet Adventure</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                china
-              </p>
-              <p>6 days</p>
-              <p>from $2100</p>
-            </div>
-          </div>
-        </article>
-
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">october 1th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <h4>best of java</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                indonesia
-              </p>
-              <p>11 days</p>
-              <p>from $1400</p>
-            </div>
-          </div>
-        </article>
-
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-3.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">september 15th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <h4>explore hong kong</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                hong kong
-              </p>
-              <p>8 days</p>
-              <p>from $5000</p>
-            </div>
-          </div>
-        </article>
-
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-4.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">december 5th, 2019</p>
-          </div>
-          <div className="tour-info">
-            <h4>kenya highlights</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                kenya
-              </p>
-              <p>20 days</p>
-              <p>from $3300</p>
-            </div>
-          </div>
-        </article>
+        {ARTICLES_INFOS.map((article) => {
+          return (
+            <article className="tour-card">
+              <div className="tour-img-container">
+                <img src={article.src} className="tour-img" alt="" />
+                <p className="tour-date">{article.date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{article.title}</h4>
+                </div>
+                <p>{article.content}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {article.country}
+                  </p>
+                  <p>{article.duration}</p>
+                  <p>{article.price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
